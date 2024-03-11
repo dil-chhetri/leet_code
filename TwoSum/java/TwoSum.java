2429fe7558f5e500c979fbdb9f8f513f839ed5c0
dil-chhetri <dilchhetri1999@gmail.com>
2024-03-11 11:11:06 +0545

    Sloved : Two Sum using java

---
+++ TwoSum/java/TwoSum.java
@@ -0,0 +1,15 @@
+
+var twoSum = function(nums, target) {
+    const numMap = new Map();
+
+    for(let i = 0; i < nums.length; i++){
+        const compliment = target - nums[i];
+        if(numMap.has(compliment)){
+            return [numMap.get(compliment), i];
+        }
+
+        numMap.set(nums[i], i);
+    }
+
+    return [];
+};
